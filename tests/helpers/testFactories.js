e13962fd0eedf75219387ce22ae8e7efaa239d6c
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2024-12-12 09:41:25 +0700

    feat : add unit testing

---
+++ tests/helpers/testFactories.js
@@ -0,0 +1,12 @@
+import LikeButtonPresenter from '../../src/scripts/utils/likebutton-presenter';
+import FavoriteRestaurantIdb from '../../src/scripts/data/favoriterestaurant-db';
+
+const createLikeButtonPresenterWithRestaurant = async (restaurant) => {
+  await LikeButtonPresenter.init({
+    likeButtonContainer: document.querySelector('#likeButtonContainer'),
+    favoriteRestaurants: FavoriteRestaurantIdb,
+    restaurant,
+  });
+};
+
+export { createLikeButtonPresenterWithRestaurant };
